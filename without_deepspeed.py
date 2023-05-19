3045d3921a1de782d89493798af9741934c7017d
seung7361 <seung7361@naver.com>
2023-05-19 06:08:05 +0000

    deepspeed success

--- without_deepspeed.py
+++ without_deepspeed.py
@@ -1,10 +1,5 @@
 import numpy as np
 import torch
-import deepspeed
-
-### initializate
-
-deepspeed.init_distributed()
 
 ### parameters from GPT
 
@@ -22,18 +17,15 @@ vocab_size = 52600
 learning_rate = 2e-5
 
 decoder_layer = torch.nn.TransformerDecoderLayer(d_model=d_model, nhead=n_heads, dropout=0.1)
-model = torch.nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=n_layers)
+model = torch.nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=n_layers).cuda()
 print('model has {:_} parameters'.format(sum(p.numel() for p in model.parameters())))
 
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
 
-model_engine, optimizer, _, _ = deepspeed.initialize(model=model, model_parameters=model.parameters(),
-                                                    optimizer=optimizer, config='ds_config.json')
-
 a = torch.randn(32, 1024).cuda()
 b = torch.randn(32, 1024).cuda()
-c = model_engine(a, b)
+c = model(a, b)
 
 ans = torch.randn(32, 1024).cuda()
 
